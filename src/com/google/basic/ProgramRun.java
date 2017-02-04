fe4b7d03d90c6eb17d9504b0d19f7e93cf185940
karthik <karthik@karthik-Inspiron-N5010>
2017-02-03 22:44:02 -0600

    Sum of even Fibonacci Series

--- src/com/google/basic/ProgramRun.java
+++ src/com/google/basic/ProgramRun.java
@@ -5,10 +5,18 @@ import java.util.Scanner;
 public class ProgramRun {
 
 	public static void main(String[] args) {
+		/*If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+		Find the sum of all the multiples of 3 or 5 below 1000.*/
+		//uncomment to run sum of all the multiples of 3 or 5
+		/*Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter the number:");
+		int num = scanner.nextInt();
+		System.out.println("the sum of multiples of 3 and 5 is:" + MultiplesOf3And5.SumOfMultiplesOf3And5(num));*/
+		
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Please enter the number:");
 		int num = scanner.nextInt();
-		System.out.println("the sum of multiples of 3 and 5 is:" + MultiplesOf3And5.SumOfMultiplesOf3And5(num));
+		System.out.println("the sum of even fib values:" + SumOfEvenValuedFibonacciNum.sumOfEvenFibNum(num));
 
 	}
 }
